1bd48dfb06177ed2df254ef258754d9db8b070f2
Rezvaneh2022 <116438852+Rezvaneh2022@users.noreply.github.com>
2022-12-08 08:47:39 +0330

    udate the time

--- src/app.js
+++ src/app.js
@@ -1,14 +1,33 @@
+function formatDate(timestamp){
+let date = new Date(timestamp);
+let hours=date.getHours();
+if (hours<10){
+    hours=`0${hours}`;
+}
+let minutes=date.getMinutes();
+if (minutes<10){
+    minutes=`0${minutes}`;
+}
+let days=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+let day= days[date.getDay()];
+return `${day} ${hours}:${minutes}` 
+}
+
+
+
 function displayTemperature(response){
     let temperatureElement=document.querySelector("temperature");
     let cityElement=document.querySelector("#city");
     let humidityNumber=document.querySelector("#humidity");
     let windNumber=document.querySelector("#wind");
     let descriptionElement=document.querySelector("#description");
+    let dateElement=document.querySelector("#date");
     temperature.innerHTML=Math.round(response.data.main.temp);
     cityElement.innerHTML=response.data.name;
     humidityNumber.innerHTML=response.data.main.humidity;
     windNumber.innerHTML=Math.round(response.data.wind.speed);
     descriptionElement.innerHTML=response.data.weather[0].description;
+    dateElement.innerHTML= formatDate(response.data.dt * 1000);
     console.log(response);
 }
 
